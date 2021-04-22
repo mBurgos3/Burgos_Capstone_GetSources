be288be0255b435d7d923c36f499a5fe26d9561b
mBurgos3 <77602708+mBurgos3@users.noreply.github.com>
2021-04-22 16:58:22 +0800

    Add files via upload

---
+++ Burgos_GetSources2/app/src/main/java/com/example/burgos_getsources/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.burgos_getsources;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button button3, button4, button5, button6;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        button3 = (Button) findViewById(R.id.button3);
+        button4 = (Button) findViewById(R.id.button4);
+        button5 = (Button) findViewById(R.id.button5);
+        button6 = (Button) findViewById(R.id.button6);
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                startActivity(intent);
+            }
+        });
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Register.class);
+                startActivity(intent);
+            }
+        });
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Nav_Time.class);
+                startActivity(intent);
+            }
+        });
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, QR_CODE_generator.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
---
+++ Burgos_GetSources2/app/src/main/java/com/example/burgos_getsources/QR_CODE_generator.java
@@ -0,0 +1,14 @@
+package com.example.burgos_getsources;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QR_CODE_generator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_q_r__c_o_d_e_generator);
+    }
+}
